5807f28d4126a092a243bbf66162650dfa0b8ef0
Daniel Ternyak <dternyak@gmail.com>
2016-01-25 02:12:21 -0600

    inital

---
+++ web/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import BaseConfig
+from models import *
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+db = SQLAlchemy(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text = request.form['text']
+        post = Post(text)
+        db.session.add(post)
+        db.session.commit()
+    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    return render_template('index.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run()
