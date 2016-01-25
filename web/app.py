f109f6ebcd7a5b47259f04c77df0cd384d5bb63e
Daniel Ternyak <dternyak@gmail.com>
2016-01-25 02:24:15 -0600

    fix db

--- web/app.py
+++ web/app.py
@@ -1,12 +1,18 @@
+# app.py
+
+
 from flask import Flask
 from flask import request, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import BaseConfig
-from models import *
+
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
+
+
+from models import *
 
 
 @app.route('/', methods=['GET', 'POST'])
